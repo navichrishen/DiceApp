d1f29de44c2fb0bc76048c21569a03034568a0f9
navichrishen <navichrishen@gmail.com>
2023-03-01 23:54:35 +0530

    first commit

---
+++ app/src/main/java/com/navindu/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.navindu.myapplication
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //val aboutBt = findViewById<Button>(R.id.android.about)
+        aboutBt()
+    }
+    fun aboutBt(){
+        aboutBt.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("20200935 Navindu")
+            builder.setMessage("I confirm that I understand what plagiarism is and have read and understood the section on Assessment Offences in the Essential Information for Students."+
+                    "The work that I have submitted is entirely my own. Any work from other authors is duly referenced and acknowledged.")
+            builder.setNeutralButton("Cancel") { dialog, id -> dialog.cancel()}
+            builder.show()
+        }
+    }
+
+}
+
